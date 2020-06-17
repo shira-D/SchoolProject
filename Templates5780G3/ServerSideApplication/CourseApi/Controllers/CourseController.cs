cd67755cc1cdeedef6c1fa1d4de5511f8477a9e7
shira-D <64360204+shira-D@users.noreply.github.com>
2020-06-17 18:41:34 +0300

    remove line

--- Templates5780G3/ServerSideApplication/CourseApi/Controllers/CourseController.cs
+++ Templates5780G3/ServerSideApplication/CourseApi/Controllers/CourseController.cs
@@ -15,7 +15,6 @@ namespace CourseApi.Controllers
         [Route("getAllCourses/{teacherid}")]
         public List<CourseDTO> GetAllCourse(int teacherid)
         {
-
            return  CourseBL.GetAllCourse(teacherid);
         }
         [HttpPost]
